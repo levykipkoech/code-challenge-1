fe1872c2346681a5b13dfafa1a03a89c6fa60530
levy kipkoech <kipkoechlevy096@gmail.com>
2022-11-27 17:24:46 +0300

    completed assignment

---
+++ speed detector/index.js
@@ -0,0 +1,12 @@
+var speed = ()=>{
+    if (speed<=70){
+        (console.log="ok")
+    }
+    else if (speed >= 71 && speed <= 75){
+         (console.log ="points 1")
+    }
+    else if  (speed >=130) {
+        (console.log ="lisence suspended") 
+    }
+    
+} 
